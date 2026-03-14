7849262b0d6469cbfa922d67b50a4c71083ba61a
IGNP Andre Pratama Milano <andremilano16@gmail.com>
2026-03-15 00:38:05 +0700

    test

---
+++ app/src/main/java/com/andre0016/aboutme/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.andre0016.aboutme
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.andre0016.aboutme.ui.theme.AboutMeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AboutMeTheme {
+                // A surface container using the 'background' color from the theme
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                ) { innerPadding ->
+                    Greeting(
+                        name = "Nama : Andre \nNIM : 607062300016 \nKelas : 47-02 \n",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = name,
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AboutMeTheme {
+        Greeting("Android")
+    }
+}
